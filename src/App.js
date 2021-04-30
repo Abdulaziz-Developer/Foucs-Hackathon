a5d9d89f1bf9e34e1425dc8e588d271c71693ced
Abdulaziz-AlQattaN <66375873+Abdulaziz-AlQattaN@users.noreply.github.com>
2021-04-30 03:23:08 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+import {useState , useRef} from "react"
+
 function App() {
+
+  const [timer, setTimer] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const increment = useRef(null)
+
+  const handleStart = () => {
+    setIsActive(true)
+    setIsPaused(true)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 10)
+    }, 1000)
+  }
+
+  const handlePause = () => {
+    clearInterval(increment.current)
+    setIsPaused(false)
+  }
+
+  const formatTime = () => {
+
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+
+
+    return ` ${getMinutes} : ${getSeconds}`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Focus and Study</h1>
+    <div className="timer-area">
+      <h1>{formatTime()}</h1>
+      <div className="buttons">
+        <button onClick={handlePause} className="stop-btn">Stop</button>
+        <button onClick={handleStart} className="start-btn" >Start</button>
+      </div>
+    </div>
     </div>
   );
+
+
 }
 
 export default App;
